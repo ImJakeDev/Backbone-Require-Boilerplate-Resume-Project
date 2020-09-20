2451731b3924a1a3edeb226fbe304bdb9d01dd57
Jake Schaffer <jake.schaffer@pxdbproject.com>
2020-09-20 14:13:45 -0500

    init and started adding new view of resume

--- public/js/app/routers/DesktopRouter.js
+++ public/js/app/routers/DesktopRouter.js
@@ -1,38 +1,31 @@
 // DesktopRouter.js
 // ----------------
-define(["jquery", "backbone", "models/Model", "views/View", "collections/Collection"],
+define([
+  "jquery",
+  "backbone",
+  "models/Model",
+  "views/View",
+  "views/Resume",
+  "collections/Collection",
+], function ($, Backbone, Model, View, Resume, Collection) {
+  var DesktopRouter = Backbone.Router.extend({
+    initialize: function () {
+      // Tells Backbone to start watching for hashchange events
+      Backbone.history.start();
+    },
 
-    function($, Backbone, Model, View, Collection) {
+    // All of your Backbone Routes (add more)
+    routes: {
+      // When there is no hash on the url, the home method is called
+      "": "index",
+    },
 
-        var DesktopRouter = Backbone.Router.extend({
+    index: function () {
+      // Instantiates a new view which will render the header text to the page
+      new Resume();
+    },
+  });
 
-            initialize: function() {
-
-                // Tells Backbone to start watching for hashchange events
-                Backbone.history.start();
-
-            },
-
-            // All of your Backbone Routes (add more)
-            routes: {
-
-                // When there is no hash on the url, the home method is called
-                "": "index"
-
-            },
-
-            index: function() {
-
-                // Instantiates a new view which will render the header text to the page
-                new View();
-
-            }
-
-        });
-
-        // Returns the DesktopRouter class
-        return DesktopRouter;
-
-    }
-
-);
+  // Returns the DesktopRouter class
+  return DesktopRouter;
+});
